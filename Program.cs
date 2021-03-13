641e8db6c162ccbecb3d7c6cd21779ef53bac2f4
Callum <dfcallum@gmail.com>
2021-03-13 19:00:51 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using LunoRepository;
+
+namespace LunoLander
+{
+    class Program
+    {
+        // kt2dhghgqdn3n
+        // YkomD6yHeCgAbe8fzbx9XbLXY9CObBgvHzrjNK16XjU
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+        static async Task Main(string[] args)
+        {
+            var lunoRepo = new LunoRepo("kt2dhghgqdn3n", "YkomD6yHeCgAbe8fzbx9XbLXY9CObBgvHzrjNK16XjU");
+
+            foreach (var repo in await lunoRepo.CoinAccount.there())
+            {
+                Console.WriteLine(repo.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
